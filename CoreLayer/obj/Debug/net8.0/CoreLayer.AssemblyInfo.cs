581f073aafe5f0ae1defec5049db6aeb75c84c61
elifr <elifrturanceng@gmail.com>
2024-08-04 12:51:48 +0300

    layers created

--- CoreLayer/obj/Debug/net8.0/CoreLayer.AssemblyInfo.cs
+++ CoreLayer/obj/Debug/net8.0/CoreLayer.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DentalHealthApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a70671826da78484bcef250dddddf58f3ca99c4")]
-[assembly: System.Reflection.AssemblyProductAttribute("DentalHealthApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DentalHealthApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("CoreLayer")]
+[assembly: System.Reflection.AssemblyTitleAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
